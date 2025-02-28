b253a62aec49a85bda5e5938ce90a37617a6cae7
Kim jimin <dkjm0318@gmail.com>
2025-02-28 15:43:00 +0900

    fix: BOJ_11501_주식 시간초과 해결

--- Jimin-K04/week01/BOJ_11501_주식.cpp
+++ Jimin-K04/week01/BOJ_11501_주식.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <map> //키-값을 쌍으로 저장하는 컨테이너
-#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -8,41 +6,31 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int T, N, stock_price, max_price;
 	long long profit;
-
-	int T, days, stock_price, index, j, cur_date;
-	map<int, int> stock_prices;
+	vector<int> stock;
 
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
-		cin >> days;
 		profit = 0;
+		cin >> N;
+		stock.clear();
 
-		for (int i = 0; i < days; i++) {
+		//일단 주가를 모두 배열에 저장함
+		for (int j = 0; j < N; j++) {
 			cin >> stock_price;
-			stock_prices.insert({ i, stock_price }); //날짜순을 키 값에 저장한다
+			stock.push_back(stock_price);
 		}
 
-		vector<pair<int, int>> sort_prices(stock_prices.begin(), stock_prices.end()); // 맵을 벡터에 복사해줌 --> 최대값 순서대로 정렬
+		max_price = stock[N - 1];
 
-		sort(sort_prices.begin(), sort_prices.end(), [](const auto& a, const auto& b) {
-			return a.second > b.second; //value 기준 내림차순 정렬
-		});
-	
-		index = 0;
-		cur_date = 0;
-		while (cur_date < days-1) { //마지막 날은 아무것도 할 수 없으므로 빼준다
-			/*가격의 최댓값이 나오는날 이전에 나왔던 모든 주식을 샀다가 그날에 판다. 
-			그 이후 또가격의 최댓값이 또 나올때까지 모두사고 최댓값이 나오는날 모두 판다..*/
-			for (j = cur_date; j < sort_prices[index].first; j++) {
-				if (sort_prices[index].second > stock_prices[j]) profit += (sort_prices[index].second - stock_prices[j]);
-			}
-			cur_date = j; //현재 날짜 갱신
-			index++; //다음 최댓값
+		//뒤에서부터 더해나가는 식으로 계산하면 시간초과 해결됨..
+		for (int i = N - 1; i >= 0; i--) {
+			if (stock[i] <= max_price) profit += (max_price - stock[i]);
+			else max_price = stock[i];
 		}
-		cout << profit << "\n";
-		stock_prices.clear(); //맵 비우기
+
+		cout << profit << '\n';
 	}
-
 }
