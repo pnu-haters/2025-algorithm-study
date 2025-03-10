4b3b02b014ed6b1c2dce298364692502673b3808
cjaewon <jwn4492@outlook.com>
2025-03-11 08:34:21 +0900

    ✏️ comment: BOJ_2531_회전 초밥 주석 수정

--- cjaewon/week03/BOJ_2531_회전 초밥.py
+++ cjaewon/week03/BOJ_2531_회전 초밥.py
@@ -6,6 +6,11 @@ from collections import deque
 
 sushi를 배열로 할지 딕셔너리로 할 지 고민했는데, 배열로 할 때 초밥 종류의 개수를 셀 때 O(N)이 걸리므로 O(1)이 걸리는 딕셔너리를 선택하였다.
 원형인 자료가 나오면 나의 경우에는 주로 deque와 rotate를 사용한다.
+
+벨트에 놓인 회전초밥 개수만 다르고 내용은 같은 문제가 있다.
+회전초밥 2 : https://www.acmicpc.net/problem/15961
+
+이 또한 똑같은 코드를 제출하니 풀림.
 """
 
 N, d, k, c = map(int, input().split())
