c0f47f1c69c46465c92bfbc7f2bb6cf63ae90967
Kim jimin <dkjm0318@gmail.com>
2025-03-23 22:57:00 +0900

    :sparkles:feat: BOJ_15989_123더하기4 구현 성공!

--- Jimin-K04/week04/BOJ_15989_123더하기4.cpp
+++ Jimin-K04/week04/BOJ_15989_123더하기4.cpp
@@ -1,28 +1,35 @@
-/*
-* n > 4 에서 부터
-* dp[n] = dp[n-1] + dp[n-3]+  dp[n-2]; //1,2,3 이 추가되는 경우를 더해줌 ->  이렇게 생각했는데 안돼서.. 이해가 힘들어요 ㅜㅜ
+/*드디어 이해했어요~
+* 숫자의 배열 순서를 1->2->3 으로 고정하기
+* 예를 들어 마지막이 2 이면 뒤에는 2,3 만 올 수 있음,,
+* 따라서 마지막 숫자가 뭔지 기록을 해야함 -> 이차원 배열 : dp[i][j] ,
+* 이때 i 는 해당 숫자이고, j 는 기록한 마지막 숫자이다.
 */
 #include <iostream>
-#include <algorithm>
-
-int dp[10000] = { 0, 1, 2, 3 }; // 배열 초기화, 뒷부분은 0으로 초기화 됨
-int find_dp(int x);
 using namespace std;
+const int MAX = 10001;
+int dp[MAX][4] = { 0, };
 
-int main() {
-	fill_n(dp+4, 9996, 1); // 뒤 요소를 모두 1로 채운다.
-	int T, n;
-	cin >> T;
-	while (T--) {
-		cin >> n;
-		cout << find_dp(n);
+void setDP() {
+	dp[1][2] = dp[1][3] = 1;
+	dp[2][2] = dp[2][3] = dp[3][2] = 2;
+	dp[3][3] = 3;
+
+	for (int n = 4; n <= 10000; n++) {
+		dp[n][1] = 1;
+		dp[n][2] = dp[n][1] + dp[n - 2][2];
+		dp[n][3] = dp[n][2] + dp[n - 3][3];
 	}
 }
 
-int find_dp(int x) {
-	if (dp[x] > 1 || x == 1) return dp[x];
-	dp[x] += find_dp(x - 2);
-	dp[x] += find_dp(x - 3);
-	return dp[x];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T, num;
+	cin >> T;
+
+	setDP();
+	while (T--) {
+		cin >> num;
+		cout << dp[num][3] << "\n";
+	}
 }
-//이것도 안되는데;; 재귀로 하면 안되는 건가...
